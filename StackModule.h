9a510bbbd95e4b68d16408b0928ca050e61d2c0b
Santiago Herrera <santherrera@frba.utn.edu.ar>
2025-06-01 19:45:19 -0300

    agrego inicializar

--- StackModule.h
+++ StackModule.h
@@ -28,14 +28,14 @@ Post condiciones: devuelve True si la pila esta llena o False en caso contrario
 */
 bool IsFull(tPila* pila);
 /*
-push
+Push
 Especificaciones: Pone un valor en la cima de la pila
 Pre condiciones: Que la pila este inicializada y que no se encuentre llena
 Post condiciones: El elemento ingresado se posiciona en la cima de la pila
 */
 void Push(tPila* pila, StackItem valor);
 /*
-pop
+Pop
 Especificaciones: Retira el valor en la cima de la pila y se lo asigna a un valor
 Pre condiciones: Que la pila este inicializada y que no se encuentre vacia
 Post condiciones: Elimina el valor del tope de la pila y devuelve el valor
