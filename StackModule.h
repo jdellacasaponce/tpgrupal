618961130288ae719d85c67e55e1c09cbfe55c48
Santiago Herrera <santherrera@frba.utn.edu.ar>
2025-06-01 19:52:21 -0300

    subo cambios

--- StackModule.h
+++ StackModule.h
@@ -4,42 +4,36 @@
 #include <stdbool.h>
 
 typedef int StackItem;
-typedef struct StackNodo* tPila;
+typedef struct Stack Stack;
 
-/*
-inicializarPila
-Especificaciones: inicializa la pila
-Pre condiciones: que exista la pila
-Post condiciones: deja la pila inicializada
-*/
-void inicializarPila(tPila* pila);
+Stack* inicializarPila();
 /*
 IsEmpty
 Especificaciones: Corrobora que la pila se encuentra vacia
 Pre condiciones: Que la pila este inicializada
 Post condiciones: devuelve True si la pila esta vacia o False en caso contrario
 */
-bool IsEmpty(tPila* pila);
+bool IsEmpty(Stack* pila);
 /*
 isFull
 Especificaciones: Corrobora que la pila se encuentre completa
 Pre condiciones: que la pila este inicializada 
 Post condiciones: devuelve True si la pila esta llena o False en caso contrario
 */
-bool IsFull(tPila* pila);
+bool IsFull(Stack* pila);
 /*
 Push
 Especificaciones: Pone un valor en la cima de la pila
 Pre condiciones: Que la pila este inicializada y que no se encuentre llena
 Post condiciones: El elemento ingresado se posiciona en la cima de la pila
 */
-void Push(tPila* pila, StackItem valor);
+void Push(Stack* pila, StackItem valor);
 /*
 Pop
 Especificaciones: Retira el valor en la cima de la pila y se lo asigna a un valor
 Pre condiciones: Que la pila este inicializada y que no se encuentre vacia
 Post condiciones: Elimina el valor del tope de la pila y devuelve el valor
 */
-int Pop(tPila* pila, StackItem* valor);
+int Pop(Stack* pila, StackItem* valor);
 
 #endif
